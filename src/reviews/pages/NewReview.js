2d12a275110046e30796a40ed8228ad34c4e3a79
Dora Jenei <ddorajenei@gmail.com>
2022-11-05 20:26:28 +0100

    fix: adding some more fields to the review form

--- src/reviews/pages/NewReview.js
+++ src/reviews/pages/NewReview.js
@@ -20,20 +20,44 @@ const NewReview = () => {
 				element="input"
 				type="text"
 				id="position"
-				label="Milyen pozíciót töltöttél be a szakmai gyakorlatod során?"
+				label="Milyen pozíciót töltöttél be a szakmai gyakorlatod során? *"
 				placeholder="Pozíció megnevezése"
 				validators={[VALIDATOR_EMPTY(), VALIDATOR_MINLEN(3)]}
 			/>
-			<Select id="department" label="Milyen területen dolgoztál?"
+			<Select id="department" label="Milyen területen dolgoztál? *"
 			options={departmentOptions} validators={[VALIDATOR_EMPTY()]}/>
 						
 					<Input
-				element="input"
+				element="textarea"
 				type="text"
-				id="position"
-				label="Milyen pozíciót töltöttél be a szakmai gyakorlatod során?"
-				placeholder="Pozíció megnevezése"
-				validators={[VALIDATOR_EMPTY(), VALIDATOR_MINLEN(3)]}
+				id="tasks"
+				label="Milyen feladatokat kellett elvégezned?"
+				validators={[]}
+			/>
+			<fieldset>
+				<legend>Kaptál a cégtől szakdolgozati témajavaslatot vagy külső konzulensi támogatást? *</legend>
+				<div>
+					<input type="radio" value="yes" name="thesis" id="thesis_yes" required/>
+					<label>Igen</label>
+					<input type="radio" value="no" name="thesis" id="thesis_no"/>
+					<label>Nem</label>
+				</div>
+			</fieldset>
+			<fieldset>
+				<legend>Ajánlott a cég lehetőséget a szakmai gyakorlat elvégzése után valamely (junior) pozíción való elhelyezkedéshez? *</legend>
+				<div>
+					<input type="radio" value="yes" name="jobOffer" id="jobOffer_yes" required/>
+					<label>Igen</label>
+					<input type="radio" value="no" name="jobOffer" id="jobOffer_no"/>
+					<label>Nem</label>
+				</div>
+			</fieldset>
+			<Input
+				element="textarea"
+				type="text"
+				id="suggestions"
+				label="Fogalmazd meg javaslataidat, észrevételeid, véleményed az eltöltött szakmai gyakorlattal kapcsolatban! "
+				validators={[]}
 			/>
 		</form>
 	);
